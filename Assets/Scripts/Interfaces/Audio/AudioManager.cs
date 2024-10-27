d23aff15e70823197cdbab1eb96ca6e3ad13654f
buinam03 <thanhks161@gmail.com>
2024-10-27 19:56:24 +0700

    Setting Audio

---
+++ Assets/Scripts/Interfaces/Audio/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("-------------Audio Source-------------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sFXSource;
+
+
+    [Header("-------------Audio Clip-------------")]
+    public AudioClip background;
+    public AudioClip click;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+    private void PlaySound()
+    {
+        sFXSource.clip = click;
+        sFXSource.PlayOneShot(click);
+    }
+}
