a04201bd72eb99c8e9678931096a4031c4da1d5f
pvta <honda43233@gmail.com>
2024-11-12 20:13:55 +0700

    tung anh sua shop ki nang dac biet

---
+++ Assets/Scripts/Data/StarSkill/StarSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New StarSO")]
+
+public class StarSO : ScriptableObject
+{
+    public int starCurrent;
+}
