da7fc2dd754c0f9c588be526696df51c4e63cb2e
Nguyen Dang Quang <nguyenquang2292003@gmail.com>
2024-12-03 21:42:18 +0700

    .

--- Assets/Scripts/AchievementSlotController.cs
+++ Assets/Scripts/AchievementSlotController.cs
@@ -10,21 +10,26 @@ public class AchievementSlotController : MonoBehaviour
     public Text titleText;       // Text tiêu đề
     public Text descriptionText; // Text mô tả
 
+    private Color achievedTitleColor = new Color(1f, 1f, 0f); // Màu vàng
+    private Color defaultIconColor = new Color(0.5f, 0.5f, 0.5f, 0.6f);// Màu xám nhạt với độ trong suốt
+    private Color defaultColor = Color.white; // Màu trắng
+
     public void UpdateAchievementStatus(bool isAchieved)
     {
+
         if (isAchieved)
         {
             // Nếu thành tựu đã đạt được, sáng lên
-            achievementIcon.color = Color.white;
-            titleText.color = new Color(1f, 1f, 0f);
-            descriptionText.color = Color.white;
+            achievementIcon.color = defaultColor;
+            titleText.color = achievedTitleColor;
+            descriptionText.color = defaultColor;
         }
         else
         {
-            // Nếu chưa đạt, làm tối nhưng vẫn nhìn thấy chữ
-            achievementIcon.color = new Color(0.5f, 0.5f, 0.5f, 0.6f); // Màu xám nhạt với độ trong suốt
-            titleText.color = new Color(1f, 1f, 1f, 0.7f); // Màu trắng nhạt để vẫn đọc được
-            descriptionText.color = new Color(1f, 1f, 1f, 0.7f); // Màu trắng nhạt để vẫn đọc được
+            // Nếu chưa đạt, làm tối icon nhưng vẫn nhìn thấy chữ
+            achievementIcon.color = defaultIconColor;
+            titleText.color = defaultColor;
+            descriptionText.color = defaultColor;
         }
     }
 }
